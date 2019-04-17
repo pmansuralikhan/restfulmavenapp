761b993e2a7f1a60bf69738cf220340668af49e2
pmansuralikhan <pmansuralikhan@gmail.com>
2019-04-17 12:30:08 +0530

    ThreadPool using ExecutorService demo

---
+++ src/main/java/com/mansur/thread/ThreadPoolDemo.java
@@ -0,0 +1,43 @@
+package com.mansur.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolDemo {
+
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(50);
+		for (int i = 0; i < 10; i++) {
+			Runnable worker = new MultiplicationTableGeneratorThread(i + 1);
+			executorService.execute(worker);
+		}
+		executorService.shutdown();
+
+		// keep on waiting executor service is terminated for all requests
+		while (!executorService.isTerminated()) {
+		}
+		System.out.println("All requests completed successfully");
+	}
+
+}
+
+class MultiplicationTableGeneratorThread implements Runnable {
+
+	private Integer number;
+
+	public MultiplicationTableGeneratorThread(Integer number) {
+		super();
+		this.number = number;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(String.format("Table for %d", new Object[] { number }));
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(String.format("%d x %d = %d", new Object[] { number, i, number * i }));
+		}
+		System.out.println();
+	}
+
+}
